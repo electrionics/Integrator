0786acb60fba22a8721bf4ec95aeca477eb98ef7
electrionics <davidlewin.dk@gmail.com>
2024-02-23 01:08:30 +0300

    Revert "Revert "Add project files.""
    This reverts commit 37f4c39d2b3c385ddddec770573895304bbf601a.

---
+++ Integrator.Data/Entities/CardTranslation.cs
@@ -0,0 +1,18 @@
+﻿namespace Integrator.Data.Entities
+{
+    public class CardTranslation
+    {
+        public int CardId { get; set; }
+
+        public string TitleEng { get; set; }
+
+        public string TitleRus { get; set; }
+
+        public string ContentEng { get; set; }
+
+        public string ContentRus { get; set; }
+
+
+        public Card Card { get; set; }
+    }
+}
---
+++ Integrator.Web.Blazor/Server/DatabaseConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Integrator.Web.Blazor.Server
+{
+    public class DatabaseConfig
+    {
+        public string ConnectionString { get; set; }
+    }
+}
